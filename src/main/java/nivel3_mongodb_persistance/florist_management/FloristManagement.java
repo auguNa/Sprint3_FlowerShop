f5c57464567501a176cc672e16962ce074e7a34b
Alba <albamrquz3@gmail.com>
2024-07-28 22:27:52 +0200

    fixing version

--- src/main/java/nivel3_mongodb_persistance/florist_management/FloristManagement.java
+++ src/main/java/nivel3_mongodb_persistance/florist_management/FloristManagement.java
@@ -1,27 +1,29 @@
 package nivel3_mongodb_persistance.florist_management;
 
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import nivel3_mongodb_persistance.connection.MongoDBConnection;
 import nivel3_mongodb_persistance.persistence.Ticket;
-import nivel3_mongodb_persistance.product_management.Decoration;
-import nivel3_mongodb_persistance.product_management.Flower;
-import nivel3_mongodb_persistance.product_management.Product;
-import nivel3_mongodb_persistance.product_management.Tree;
+import nivel3_mongodb_persistance.product_management.*;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class FloristManagement {
-    private MongoDatabase database = MongoDBConnection.getDatabase();
-    private MongoCollection<Document> products = database.getCollection("products");
-    private MongoCollection<Document> tickets = database.getCollection("tickets");
+    private final MongoDatabase database = MongoDBConnection.getDatabase();
+    private final MongoCollection<Document> products = database.getCollection("products");
+    private final MongoCollection<Document> tickets = database.getCollection("tickets");
 
-    private String name;
-    private ArrayList<Product> stock;
-    private ArrayList<Ticket> sales;
+    private final String name;
+    private final ArrayList<Product> stock;
+    private final ArrayList<Ticket> sales;
 
     public FloristManagement(String name) {
         this.stock = new ArrayList<>();
@@ -41,25 +43,76 @@ public class FloristManagement {
 
     public void addProduct(Product product) {
 
-        Document productToAdd = new Document()
-                .append("counter", product.getCounter())
-                .append("id", product.getId())
-                .append("price", product.getPrice())
-                .append("attribute", product.getAttribute());
+        try {
+            String type = product.getClass().getSimpleName().toLowerCase();
 
-        products.insertOne(productToAdd);
-        stock.add(product);
+            Document productToAdd = new Document()
+                    .append("counter", product.getCounter())
+                    .append("id", product.getId())
+                    .append("price", product.getPrice())
+                    .append("type", type)
+                    .append("attribute", product.getAttribute());
+
+            products.insertOne(productToAdd);
+            stock.add(product);
+        } catch (Exception e) {
+            System.out.println("Error adding product: " + e.getMessage());
+        }
+    }
+
+    private ObjectId getProductById(int id) {
+        try {
+            Bson filter = Filters.eq("id", id);
+            Document productId = products.find(filter).first();
+
+            if (productId != null) {
+                return productId.getObjectId("_id");
+            } else {
+                System.out.println("Product not found with ID: " + id);
+                return null;
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error retrieving product: " + e.getMessage());
+            return null;
+        }
     }
 
     public void removeProduct(Product product) {
-        Document productToRemove = new Document("id", product.getId());
-        products.deleteOne(productToRemove);
-        stock.remove(product);
+        try {
+            Document productToRemove = new Document("id", product.getId());
+            products.deleteOne(productToRemove);
+            stock.remove(product);
+        } catch (Exception e) {
+            System.out.println("Error removing product: " + e.getMessage());
+        }
     }
 
     public void addSale(Ticket ticket) {
+        if (ticket == null || ticket.getProducts() == null) {
+            System.out.println("Adding a sale with a null ticket or null products list is not allowed.");
+            return;
+        }
 
-        sales.add(ticket);
+        try {
+            List<ObjectId> productObjectIds = ticket.getProducts().stream()
+                    .map(product -> getProductById(product.getId()))
+                    .filter(Objects::nonNull)
+                    .toList();
+
+            if (productObjectIds.isEmpty()) {
+                System.out.println("No valid products found");
+                return;
+            }
+
+            Document ticketToAdd = new Document("products", productObjectIds);
+            tickets.insertOne(ticketToAdd);
+            sales.add(ticket);
+            products.deleteMany(Filters.in("_id", productObjectIds));
+        } catch (Exception e) {
+            System.out.println("Error adding sale: " + e.getMessage());
+        }
+
     }
 
     public List<Ticket> getSales() {
@@ -77,28 +130,75 @@ public class FloristManagement {
 
 
     private void loadStock() {
+        stock.clear();
+
         try (MongoCursor<Document> cursor = products.find().iterator()) {
             while (cursor.hasNext()) {
                 Document doc = cursor.next();
                 int id = doc.getInteger("id");
                 double price = doc.getDouble("price");
                 String attribute = doc.getString("attribute");
+                String type = doc.getString("type");
 
                 if(doc.getString("type").equals("tree")) {
-                    Product product = new Tree(price, 0);
+                    Product product = new Tree(price, Double.parseDouble(attribute));
                     stock.add(product);
                 } else if(doc.getString("type").equals("flower")) {
                     Product product = new Flower(price, attribute);
                     stock.add(product);
                 } else if(doc.getString("type").equals("decoration")) {
-                    Product product = new Decoration(price, null);
+                    Product product = new Decoration(price, Objects.equals(attribute, "WOOD") ? Material.WOOD : Material.PLASTIC);
                     stock.add(product);
                 }
             }
         }
     }
 
-    private void loadSales() {
+    public Product getProductByObjectId(ObjectId objectId) {
+        try {
+            Document productDoc = products.find(new Document("_id", objectId)).first();
+            if (productDoc != null) {
+                String type = productDoc.getString("type");
+                double price = productDoc.getDouble("price");
+                String attribute = productDoc.getString("attribute");
 
+                return switch (type) {
+                    case "tree" -> new Tree(price, Double.parseDouble(attribute));
+                    case "flower" -> new Flower(price, attribute);
+                    case "decoration" ->
+                            new Decoration(price, Objects.equals(attribute, "WOOD") ? Material.WOOD : Material.PLASTIC);
+                    default -> null;
+                };
+            } else {
+                System.out.println("Product not found");
+                return null;
+            }
+        } catch (Exception e) {
+            System.out.println("Error retrieving product");
+            return null;
+        }
+    }
+
+
+    private void loadSales() {
+        sales.clear();
+
+        FindIterable<Document> salesDocuments = tickets.find();
+
+        for (Document saleDoc : salesDocuments) {
+            List<ObjectId> productObjectIds = saleDoc.getList("products", ObjectId.class);
+
+            List<Product> productsList = new ArrayList<>();
+            for (ObjectId objectId : productObjectIds) {
+                Product product = getProductByObjectId(objectId);
+                if (product != null) {
+                    productsList.add(product);
+                }
+            }
+
+            Ticket ticket = new Ticket(productsList);
+
+            sales.add(ticket);
+        }
     }
 }
